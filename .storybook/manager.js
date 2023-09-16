8c48dd46c01f6e63c8314a2053a2252bdd1729db
Hadi Samadzad <h.samadzad@gmail.com>
2023-09-16 01:11:34 +0100

    Update images

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -4,8 +4,8 @@ import { addons } from '@storybook/addons';
 addons.setConfig({
   theme: {
     ...themes.dark,
-    brandImage: 'https://hamishw.com/icon.svg',
+    brandImage: 'https://maryamsarafrazi.com/icon.svg',
     brandTitle: 'Hamish Williams Components',
-    brandUrl: 'https://hamishw.com',
+    brandUrl: 'https://maryamsarafrazi.com',
   },
 });
